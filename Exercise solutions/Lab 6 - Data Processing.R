0190a1213bab2c6161bf7710b18875b2f8d283a4
xyc9471 <xyc9471@users.noreply.github.com>
2019-02-13 17:05:24 -0500

    Exercise solutions of Lab 6
    Deriving from the master script, this script includes the solution codes to all 12 exercises in Lab 6.

--- Exercise solutions/Lab 6 - Data Processing.R
+++ Exercise solutions/Lab 6 - Data Processing.R
@@ -1,23 +1,53 @@
+#------------------------------------------------------------------------------ #
+#                                                                               #
+#                                    DIME                                       #
+#                              Data Processing                                  #
+#                         master script w/ solutions                            #
+#                                                                               #
+#------------------------------------------------------------------------------ #
 
+### Setup -----------------------------------------------------------------------
 
-# for (year in 2015:2017) {
-#   download.file(paste0("https://www.kaggle.com/unsdsn/world-happiness/downloads/", year ,".csv/2"),
-#                 destfile = file.path(Data,paste0("Raw/WHR", year, ".csv")), 
-#                 method = "curl") # method = "curl" [mac only for https]
-#   
-# }
+# 1) setup work directory
+projectFolder <- file.path("C:/Users/WB546716/Documents/GitHub/dime-r-training")
+dataWorkFolder    <- file.path(projectFolder,"DataWork")
+Data              <- file.path(dataWorkFolder,"DataSets")
+finalData         <- file.path(Data,"Final")
+rawData           <- file.path(Data,"Raw")
+
+# 2) download the data
+for (year in 2015:2017) {
+  download.file(paste0("https://www.kaggle.com/unsdsn/world-happiness/downloads/", year ,".csv/2"),
+                destfile = file.path(Data,paste0("Raw/WHR", year, ".csv")),
+                method = "curl")
+}
+#### the website requires registration to download the data set, so this link might not work
 
 dateDownloaded <- date()
 
-whr15 <- read.csv(file.path(Data,"Raw/WHR2015.csv"),
+# 3) load packages
+library(ggplot2)
+library(tibble)
+library(tidyr)
+library(readr)
+library(purrr)
+library(tidyverse)
+library(dplyr)
+
+### Exercise 1: Loading a Data Set from CSV ----------------------------------------
+whr15 <- read.csv(file.path(rawData,"2015.csv"),
                   header = T,
                   stringsAsFactors = F)
-whr16 <- read.csv(file.path(Data,"Raw/WHR2016.csv"),
+whr16 <- read.csv(file.path(rawData,"2016.csv"),
                   header = T,
                   stringsAsFactors = F)
-whr17 <- read.csv(file.path(Data,"Raw/WHR2017.csv"),
+whr17 <- read.csv(file.path(rawData,"2017.csv"),
                   header = T,
                   stringsAsFactors = F)
+
+### Exploring a Data Set -----------------------------------------------------------
+
+### Exercise 2: exploring the data -------------------------------------------------
 View(whr15)
 class(whr15)
 dim(whr15)
@@ -25,51 +55,61 @@ str(whr15)
 head(whr15)
 summary(whr15)
 
-
 str(whr15)
 str(whr16)
 str(whr17)
+
+### ID Variables -------------------------------------------------------------------
+dim(whr15) # checking for obs and #of variables
+dim(whr16)
+dim(whr17)
 
 n_distinct(whr15$Country, na.rm = TRUE)
 n_distinct(whr16$Country, na.rm = TRUE)
 n_distinct(whr17$Country, na.rm = TRUE)
 
+### Exercise 3: identify the ID ----------------------------------------------------
+n_distinct(whr15$Region, na.rm = TRUE)
+n_distinct(whr15$Country, na.rm = TRUE)
+
+n_distinct(whr16$Country, na.rm = TRUE) == nrow(whr16)
+n_distinct(whr17$Country, na.rm = TRUE) == nrow(whr17)
+
+### Exercise 4: compare vectors ----------------------------------------------------
 setdiff(whr15$Country, whr16$Country)
 setdiff(whr16$Country, whr15$Country)
 setdiff(whr16$Country, whr17$Country)
 setdiff(whr17$Country, whr16$Country)
 
+### Exercise 5: replacing values ---------------------------------------------------
 
 whr15$Country[whr15$Country == "Somaliland region"]         <- "Somaliland Region" 
 whr17$Country[whr17$Country == "Hong Kong S.A.R., China"]   <- "Hong Kong" 
 whr17$Country[whr17$Country == "Taiwan Province of China"]  <- "Taiwan" 
-
-
+# checking for names
 names(whr15)
 names(whr16)
 
-
+### Exercise 6: creating variables -------------------------------------------------
 whr15$year <- 2015
 whr16$year <- 2016
 whr17$year <- 2017
 
-whr_panel <- rbind(whr15, whr16, whr17)
+### Appending and merging data sets ------------------------------------------------
+
+### Exercise 7: append data sets ---------------------------------------------------
+whr_panel <- rbind(whr15, whr16, whr17) # this one doesnt run
 whr_panel <- bind_rows(whr15, whr16, whr17)
 
+# checking for variables again
 setdiff(names(whr15), names(whr16))
 setdiff(names(whr16), names(whr15))
 setdiff(names(whr17), names(whr16))
 setdiff(names(whr16), names(whr17))
 
-whr17 <- rename(whr17, 
-              Lower.Confidence.Interval = Whisker.low,
-              Upper.Confidence.Interval = Whisker.high)
-
-whr_panel <- rbind(whr15, whr16, whr17)
-whr_panel <- bind_rows(whr15, whr16, whr17)
-
-
-
+### Exercise 8: subset the data ----------------------------------------------------
+regions <- select(whr16, Country, Region)
+str(regions)
 keepVars <-   c("Country",
                 "Region",
                 "year",
@@ -82,84 +122,76 @@ keepVars <-   c("Country",
                 "Trust..Government.Corruption.",
                 "Generosity",
                 "Dystopia.Residual")
-
-whr15 <- select(whr15, keepVars)
+whr15 <- whr15[, keepVars]
 whr16 <- select(whr16, keepVars)
-
-regions <- select(whr16, Country, Region)
-
-
-whr17 <- left_join(whr17, regions)
 whr17 <- select(whr17, keepVars)
 
-whr_panel <- rbind(whr15, whr16, whr17)
+### Exercise 9: Merge -------------------------------------------------------------
+whr17 <- left_join(whr17, regions) # this doesnt run, so we have to fix the dataset first
 
-names(whr_panel) <-  c("country",
-                       "region",
-                       "year",
-                       "happy_rank",
-                       "happy_score",
-                       "gdp_pc",
-                       "family",
-                       "health",
-                       "freedom",
-                       "trust_gov_corr",
-                       "generosity",
-                       "dystopia_res")
+# fixing data sets
+str(whr17)
+any(is.na(whr17$Region)) # checking for missing
+sum(is.na(whr17$Region)) # checking for #of missing
+whr17$Country[is.na(whr17$Region)] # shows where the missing is
+whr17$Region[whr17$Country %in% c("Mozambique", "Lesotho", "Central African Republic")] <- "Sub-Saharan Africa"
+any(is.na(whr17$Region))
 
+# rename some variables
+whr17 <- rename(whr17,
+                Lower.Confidence.Interval = Whisker.low,
+                Upper.Confidence.Interval = Whisker.high)
 
-any(is.na(whr_panel$region))
-sum(is.na(whr_panel$region))
-whr_panel$country[is.na(whr_panel$region)]
+names(whr15)
+newnames <-  c("country",
+            "region",
+            "year",
+            "happy_rank",
+            "happy_score",
+            "gdp_pc",
+            "family",
+            "health",
+            "freedom",
+            "trust_gov_corr",
+            "generosity",
+            "dystopia_res")
+names(whr15) <- newnames
 
-whr_panel$region[whr_panel$country == "Mozambique"]               <- "Sub-Saharan Africa"
-whr_panel$region[whr_panel$country == "Lesotho"]                  <- "Sub-Saharan Africa"
-whr_panel$region[whr_panel$country == "Central African Republic"] <- "Sub-Saharan Africa"
+# Ordering variables
+whr17 <- select(whr17, keepVars)
+names(whr16) <- newnames
+names(whr17) <- newnames
 
-any(is.na(whr_panel$region))
+## Append the data
+whr_panel <- rbind(whr15, whr16, whr17) # the rbind works now!
 
-str(whr_panel$region)
-whr_panel$region_cat <- as.factor(whr_panel$region)
-str(whr_panel$region_cat)
+### Saving a data set -----------------------------------------------------------
+
+### Exercise 10: save the data set as csv ---------------------------------------
+write.csv(whr_panel,
+          file.path(finalData, "whr_panel.csv"),
+          row.names = FALSE)
+
+### Adding Variables ------------------------------------------------------------
+
+### Exercise 11: Create a variable based on a formula ---------------------------
+whr_panel <- 
+  mutate(whr_panel,
+         happy_high = happy_score > median(happy_score))
+
+whr_panel <- 
+  mutate(whr_panel,
+         happy_high = happy_score > median(happy_score),
+         happy_low = happy_score < median(happy_score),
+         dystopia_res = NULL)
+
+### Exercise 12: turn a variable into a factor ----------------------------------
+whr_panel <-
+  mutate(whr_panel,
+         region_cat = factor(region))
+
+class(whr_panel$region_cat)
+
 levels(whr_panel$region_cat)
-table(whr_panel$region_cat)
 
-whr_panel$region_ord <- factor(whr_panel$region,
-                     levels = c("Australia and New Zealand",
-                                "Central and Eastern Europe",
-                                "Western Europe",
-                                "Latin America and Caribbean",
-                                "North America",
-                                "Southeastern Asia",
-                                "Southern Asia",
-                                "Eastern Asia",
-                                "Middle East and Northern Africa",
-                                "Sub-Saharan Africa"),
-                     ordered = T)
-table(whr_panel$region_ord)
 
-whr_panel <- mutate(whr_panel, happy_high = happy_score > median(happy_score))
-
-whr_panel$happy <- factor(whr_panel$happy_high,
-                         levels = c(FALSE, TRUE),
-                         labels = c("Not so happy", "Happy"))
-
-happy_panel <- filter(whr_panel, happy == "Happy")
-
-# Reshaping
-whr_happy <- select(whr_panel, country, region, year, happy)
-
-whr_happy_wide <- spread(whr_happy,
-                         key = year,
-                         value = happy)
-
-whr_happy_long <- gather(whr_happy_wide,
-                         key = year,
-                         value = happy,
-                         `2015`, `2016`, `2017`)
-
-whr_happy <- select(whr_panel, country, region, year, happy, happy_score)
-
-whr_hapy_reshape <- reshape(whr_happy,
-                            v.names = "year",
-                            )
