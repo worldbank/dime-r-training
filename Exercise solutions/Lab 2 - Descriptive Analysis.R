2fbf905b077ff7e832c4d0c07eae3fe954f2cab2
xyc9471 <xyc9471@users.noreply.github.com>
2019-02-21 16:16:43 -0500

    Revised exercise solution of Lab 2
    Fixed the errors in old version.

--- Exercise solutions/Lab 2 - Descriptive Analysis.R
+++ Exercise solutions/Lab 2 - Descriptive Analysis.R
@@ -2,123 +2,213 @@
 #                                                                                #
 #                                     DIME                                       #
 #                          Descriptive Staticstics in R                          #
+#                             Exercise Solutions                                 #
 #                                                                                #
 # ------------------------------------------------------------------------------ #
 
 
-# Set up ----------------------------------------------------------------------
+# Set up -----------------------------------------------------------------------
   
-  # Install stargazer
-  install.packages("stargazer",
+# Install stargazer
+install.packages("stargazer",
                    dependencies = TRUE)
+install.packages("xlsx",
+                 dependencies = TRUE)
 
-  # Load stargazer
-  library(stargazer)
+# Load stargazer
+library(stargazer)
+library(tidyr)
+library(xlsx)
 
-  lwh <- read.csv(file.path(finalData,"lwh_clean.csv"),
+# set work directory
+projectFolder <- file.path("C:/Users/WB546716/Documents/GitHub/dime-r-training")
+dataWorkFolder    <- file.path(projectFolder,"DataWork")
+Data              <- file.path(dataWorkFolder,"DataSets")
+finalData         <- file.path(Data,"Final")
+rawOutput         <- file.path(dataWorkFolder,"Output","Raw")
+
+# load the data set
+whr <- read.csv(file.path(finalData,"whr_panel.csv"),
                   header = T)
-  
+# view data set before any descriptive analysis
+View(whr)
+
 # EXERCISE 1: Summary statistics ------------------------------------------------
   
-  summary(lwh)
+summary(whr)
   
 # EXERCISE 2: Frequencies tables ------------------------------------------------
   
-  # Year of data collection
-  table(lwh$year)
+# Year of data collection
+table(whr$year)
   
-  # Gender of household head per year
-  table(lwh$gender_hhh, lwh$year)
-  
+# Number of countries per region per year
+table(whr$region, whr$year)
+
+# BONUS EXERCISE ----------------------------------------------------------------
+
+# restrict the observations to only 2017
+whr17 <- whr[whr$year == 2017,]
+
+# table with a boolean vector
+table(whr17$region,
+      whr17$happy_rank > mean(whr17$happy_rank)) # whose happy rank is above average
+
 # EXERCISE 3: Stargazer ---------------------------------------------------------
   
-  # A descriptive table with stargazer
-  stargazer(lwh,
-            digits = 1,
-            type = "text")
+# A descriptive table with stargazer
+stargazer(whr,
+          digits = 1,
+          type = "text")
   
-# EXERCISE 4: Export tables to LaTeX --------------------------------------------
+# EXERCISE 4  : create vectors --------------------------------------------------
   
-  # Vector with covariates to be kept
-  covariates <- c("age_hhh",
-                  "num_dependents",
-                  "income_total_win",
-                  "expend_food_yearly")
-  # subset lwh
-  lwh_simp <- lwh[, covariates]
-  
-  # Set labels
-  cov_labels <- c("Age of household head", "Number of dependents",
-                  "Anual income (winsorized)", "Yearly food expediture")
-  # Save table to latex
-  stargazer(lwh_simp,
-            covariate.labels = cov_labels,
-            summary.stat = c("n", "mean", "sd", "min", "max"),
-            digits = 1,
-            out = file.path(rawOutput,"desc_table.tex"))
-  
-  
-# EXERCISE 5: Aggregate ---------------------------------------------------------
-  
-  # Aggregate income by year and treatment status
-  year_inc_tab <-
-    aggregate(x = lwh$income_total_win,
-              by = list(year = lwh$year,
-                        treatment = lwh$treatment_hh),
-              FUN = mean)
-  
+# Vector with covariates to be kept
+covariates <- c("happy_score",
+                "gdp_pc",
+                "freedom",
+                "trust_gov_corr")
+# subset whr
+whr_simp <- whr[, covariates]
 
-# EXERCISE 6: Reshape -----------------------------------------------------------
-  
-  # Aggregate income by year and treatment status
-  year_inc_tab <- reshape(year_inc_tab,
-                          idvar = "treatment",
-                          timevar = "year",
-                          direction = "wide")
-  
-# EXERCISE 7: Create table from scratch -----------------------------------------
-  
-  # Label variables
-  column_lab <- c("Treatment status", "2012", "2013", "2014", "2016", "2018")
-  # Create table
-  stargazer(year_inc_tab,
-            summary = F,
-            # Some extra formatting:
-            covariate.labels = column_lab,
-            title = "Total income by treatment status and year",
-            header = F,
-            digits = 1,
-            rownames = F)
+# EXERCISE 5: EXPORT TABLES TO LATEX
+
+# Set labels
+cov_labels <- c("Happy score", "GDP per capita",
+                  "Freedom", "Trust in government and corruption")
+
+# Save table to latex
+stargazer(whr_simp,
+          covariate.labels = cov_labels, # formatting the table
+          summary.stat = c("n", "mean", "sd", "min", "max"), # choose which stats to display
+          digits = 2,
+          out = file.path(rawOutput,"desc_table.tex"))
   
   
-# EXERCISE 8: Export table to Excel ---------------------------------------------
+# EXERCISE 6: Aggregate ---------------------------------------------------------
   
-  write.table(year_inc_tab,
-              sep = ",",
-              row.names = F,
-              col.names = c("Treatment status",
-                            "2012", "2013", "2014", "2016", "2018"),
-              file = file.path(rawOutput, "year_inc_tab.csv"))
+# Aggregate happy score by region and year 
+happy_table <- aggregate(happy_score ~ year + region,
+                         data = whr,
+                         FUN = mean)
+View(happy_table)
   
-# BONUS: Export a regression table ----------------------------------------------
+# EXERCISE 7: Reshape -----------------------------------------------------------
+
+# reshape into wide on year
+happy_table <- spread(happy_table,
+                      key = year,
+                      value = happy_score)
+View(happy_table)
+
+# EXERCISE 8: Export the table to latex -----------------------------------------
   
-  # Run a Regression
-  reg1 <- lm(expend_food_yearly ~
-               income_total_win + num_dependents,
-             data = lwh)
+stargazer(happy_table,
+          summary = F,
+          out = file.path(rawOutput, "happy_table.tex"), # exporting
+          title = "Happy table", # formatting the table 
+          digits = 1,
+          rownames = F)
+
+# CHALLENGE EXERCISE ------------------------------------------------------------
+
+#### Construct a duplicated df
+x1 <- whr
+x1$stat <- "mean"
+
+x2 <- whr
+x2$stat <- "N"
+
+x12 <- rbind(x1,x2)
+
+#### Collapse
+happy_table2 <-
+  aggregate(happy_score ~ stat +  region + year,
+            data = x12,
+            FUN = mean)
+
+happy_table2 <- 
+  rename(happy_table2, c("happy_score" = "value"))
+
+#### Freq df
+
+freqdf <- 
+  as.data.frame(table(whr$region, whr$year))
+
+#### replace values
+happy_table2$value[happy_table2$stat == "N"] <- freqdf$Freq
+
+happy_table2 <- 
+  select(happy_table2, region, year, stat, value)
+
+#### Reshape
+ht_wd <- 
+  spread(happy_table2, 
+         key = year, 
+         value = value)
+
+#### Cosmetics
+
+ht_wd$region[as.integer(row.names(ht_wd)) %% 2 == 0] <- "" # delete the duplicates of region names
+
+#### Exporting the table to latex
+stargazer(ht_wd, 
+          summary = F,
+          rownames = F) 
+
+# EXERCISE 9: Save data frame in xlsx format -----------------------------------------
+
+write.xlsx(happy_table,
+           file = file.path(rawOutput,"happy_table.xlsx"))
+
+
   
-  # Set labels
-  depvar_label <- "Yearly food expenditure (winsorized)"
-  covar_labels <- c("Total income (winsorized)",
-                    "Number of dependents")
-  
-  # Export regression table
-  stargazer(reg1,
-            title = "Regression table",
-            dep.var.labels = depvar_label,
-            covar_labels = covar_labels,
-            digits = 2,
-            header = F)
-  
+# BONUS: Export a regression table ---------------------------------------------------
+
+# load the data set
+View(iris)
+
+# Run a Regression
+reg1 <- lm(Sepal.Length ~ Sepal.Width,
+           data = iris)
+
+# Export a regression table
+depvar_label <- "Sepal Length"
+covar_labels <- "Sepal Width"
+
+stargazer(reg1,
+          title = "Regression table",
+          dep.var.labels=depvar_label,
+          covariate.labels=covar_labels,
+          digits = 2,
+          out = file.path(rawOutput, "iris_regression.tex"), # exporting the table to latex
+          header = F)
+
+### multiple regression results in one table
+
+# reg with two independent variables
+reg2 <- lm(Sepal.Length ~ Sepal.Width + Petal.Length,
+           data = iris)
+# reg with two indep vars and species FE
+reg3 <- lm(Sepal.Length ~ Sepal.Width + Petal.Length + factor(Species),
+           data = iris)
+
+depvar_label <- "Sepal Length"
+covar_labels <- c("Sepal Width",
+                  "Petal Length")
+
+# formatting the table
+stargazer(reg1,
+          reg2,
+          reg3,
+          font.size = "tiny",
+          title = "Regression table",
+          keep = c("Spatial.Width", "Petal.Length"),
+          dep.var.labels = depvar_label,
+          covariate.labels = covar_labels,
+          add.lines = list(c("Species FE", "No", "No", "Yes")),
+          omit.stat = c("ser"),
+          digits = 2,
+          header = F)
   
 #=============================== The end ======================================#
